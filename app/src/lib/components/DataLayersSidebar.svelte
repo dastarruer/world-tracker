0d1f132085fdf5c9fba2f27f977b851d4309d450
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-03-13 20:25:34 -0400

    Add .coderabbit.yaml file

---
+++ app/src/lib/components/DataLayersSidebar.svelte
@@ -0,0 +1,21 @@
+<aside>
+	<h2>Data Layers</h2>
+	<ul>
+		<li>
+			<label>
+				<input type="checkbox" name="layer" value="planes" checked />
+				Satellites
+			</label>
+		</li>
+	</ul>
+</aside>
+
+<style>
+  aside {
+    background-color: var(--bg);
+    color: var(--text);
+
+    width: 400px;
+    height: 700px;
+  }
+</style>
