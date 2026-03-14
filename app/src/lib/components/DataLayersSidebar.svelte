5f1bbf233ed1ac5dc8980c5c002179a41c154b27
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-03-13 21:39:21 -0400

    Make data layers scrollable

--- app/src/lib/components/DataLayersSidebar.svelte
+++ app/src/lib/components/DataLayersSidebar.svelte
@@ -1,23 +1,31 @@
 <aside>
 	<h2>Data Layers</h2>
 	<ul>
+  <!-- Here for testing purposes as of now; will be removed later -->
+  {#each Array(50) as _, i}
 		<li>
 			<label>
 				<input type="checkbox" name="layer" value="planes" checked />
 				Satellites
 			</label>
 		</li>
+    {/each}
 	</ul>
 </aside>
 
 <style>
-  aside {
-    background-color: var(--bg-alt);
-    color: var(--text);
-
+	aside {
     width: 400px;
+
+    background-color: var(--bg-alt);
+		color: var(--text);
+	}
+
+	ul {
     height: auto;
     max-height: 80vh;
     overflow-y: auto;
-  }
+		display: flex;
+		flex-flow: column nowrap;
+	}
 </style>
