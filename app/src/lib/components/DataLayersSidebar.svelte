dd14581091f2911308c02f2a2f56c0ad6ed762f0
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-03-13 20:15:43 -0400

    Change height based on number of checkboxes

---
+++ app/src/lib/components/DataLayersSidebar.svelte
@@ -0,0 +1,23 @@
+<aside>
+	<h2>Data Layers</h2>
+	<ul>
+		<li>
+			<label>
+				<input type="checkbox" name="layer" value="planes" checked />
+				Satellites
+			</label>
+		</li>
+	</ul>
+</aside>
+
+<style>
+  aside {
+    background-color: var(--bg);
+    color: var(--text);
+
+    width: 400px;
+    height: auto;
+    max-height: 80vh;
+    overflow: hidden;
+  }
+</style>
