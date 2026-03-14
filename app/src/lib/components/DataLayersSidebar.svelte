4fbf1c3eef58b6fcac74c21bb366e2549a10e52f
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-03-13 21:45:35 -0400

    Add padding + margin to li elements

--- app/src/lib/components/DataLayersSidebar.svelte
+++ app/src/lib/components/DataLayersSidebar.svelte
@@ -1,31 +1,38 @@
 <aside>
 	<h2>Data Layers</h2>
 	<ul>
-  <!-- Here for testing purposes as of now; will be removed later -->
-  {#each Array(50) as _, i}
-		<li>
-			<label>
-				<input type="checkbox" name="layer" value="planes" checked />
-				Satellites
-			</label>
-		</li>
-    {/each}
+		<!-- Here for testing purposes as of now; will be removed later -->
+		{#each Array(50) as _, i}
+			<li>
+				<label>
+					<input type="checkbox" name="layer" value="planes" checked />
+					Satellites
+				</label>
+			</li>
+		{/each}
 	</ul>
 </aside>
 
 <style>
 	aside {
-    width: 400px;
+		width: 400px;
 
-    background-color: var(--bg-alt);
+		background-color: var(--bg-alt);
 		color: var(--text);
 	}
 
 	ul {
-    height: auto;
-    max-height: 80vh;
-    overflow-y: auto;
 		display: flex;
 		flex-flow: column nowrap;
+
+		height: auto;
+		max-height: 80vh;
+		overflow-y: auto;
+
 	}
+
+  li {
+    padding: 5px;
+    margin: 5px;
+  }
 </style>
