b3c928af18581230682f8ac736b8c74d97546e89
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-03-16 13:42:57 -0400

    Use bits UI's scrollarea

--- app/src/lib/components/data-layers/DataLayersSidebar.svelte
+++ app/src/lib/components/data-layers/DataLayersSidebar.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
 	import LayerSwitch from './LayerSwitch.svelte';
+	import LayerScrollArea from './LayerScrollArea.svelte';
 </script>
 
 <aside>
 	<h1>Data Layers</h1>
 	<hr />
-	<ul>
-		<!-- Here for testing purposes as of now; will be removed later -->
-		{#each Array(50) as _, i}
-			<li>
+	<LayerScrollArea>
+		{#snippet layers()}
+			<!-- Here for testing purposes as of now; will be removed later -->
+			{#each Array(50) as _, i}
 				<LayerSwitch label="Satellites" />
-			</li>
-		{/each}
-	</ul>
+			{/each}
+		{/snippet}
+	</LayerScrollArea>
 </aside>
 
 <style>
@@ -22,7 +23,7 @@
 
 		width: 400px;
 
-    border: 1px solid var(--bg-alt);
+		border: 1px solid var(--bg-alt);
 		border-radius: 10px;
 
 		background-color: var(--bg);
@@ -46,44 +47,5 @@
 		opacity: 0.1;
 		width: 370px;
 		align-self: center;
-	}
-
-	ul {
-		list-style: none;
-
-		display: flex;
-		flex-flow: column nowrap;
-
-		height: auto;
-		max-height: 80vh;
-		overflow-y: auto;
-
-		/* Space between divider and list */
-		padding: 12px 0;
-
-		/* Add some spacing between scrollbar and bottom of sidebar */
-		margin-bottom: 12px;
-
-		/* Set scrolbar track and scrollbar to transparent */
-		scrollbar-color: transparent transparent;
-		scrollbar-width: thin;
-		scrollbar-gutter: stable;
-	}
-
-	aside:hover ul {
-		scrollbar-color: rgba(255, 255, 255, 0.15) transparent;
-	}
-
-	li {
-		padding: 5px 16px;
-		margin: 5px;
-	}
-
-	li:first-child {
-		margin-top: 0;
-	}
-
-	li:last-child {
-		margin-bottom: 0;
 	}
 </style>
---
+++ app/src/lib/components/data-layers/LayerScrollArea.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { ScrollArea } from 'bits-ui';
+
+	let {
+		layers
+	}: {
+		layers: Snippet;
+	} = $props();
+</script>
+
+<ScrollArea.Root type="hover">
+	{#snippet child({ props })}
+		<div class="scroll-root" {...props}>
+			<ScrollArea.Viewport class="viewport">
+				{#snippet children()}
+					<div class="list-container">
+						{@render layers()}
+					</div>
+				{/snippet}
+			</ScrollArea.Viewport>
+			<ScrollArea.Scrollbar orientation="vertical">
+				{#snippet child({ props: scrollRootProps })}
+					<div class="scrollbar-root" {...scrollRootProps}>
+						<ScrollArea.Thumb>
+							{#snippet child({ props: scrollThumbProps })}
+								<div class="scrollbar-thumb" {...scrollThumbProps}></div>
+							{/snippet}
+						</ScrollArea.Thumb>
+					</div>
+				{/snippet}
+			</ScrollArea.Scrollbar>
+		</div>
+	{/snippet}
+</ScrollArea.Root>
+
+<style>
+	.scroll-root {
+		height: auto;
+		max-height: 80vh;
+
+		display: flex;
+		flex-direction: column;
+		overflow: hidden;
+		padding: 12px 0;
+		margin-bottom: 12px;
+	}
+
+	.scrollbar-root {
+		width: 3px;
+		background: rgba(0, 0, 0, 0.05);
+		margin: 4px;
+		transition: background 0.2s;
+	}
+
+	.scrollbar-thumb {
+		flex: 1;
+		background: var(--bg-alt);
+		border-radius: 10px;
+		position: relative;
+	}
+
+	.list-container {
+		list-style: none;
+		display: flex;
+		gap: 20px;
+		flex-flow: column nowrap;
+		margin: 0;
+		padding: 15px;
+	}
+
+	.list-container:first-child {
+		margin-top: -10px;
+	}
+
+	.list-container:last-child {
+		margin-bottom: -10px;
+	}
+</style>
