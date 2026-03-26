b4e851d3bb7671be42e4dd9535358b4dcb9c8f79
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-03-25 20:14:36 -0400

    Add normal map

--- app/src/lib/components/globe/Globe.svelte
+++ app/src/lib/components/globe/Globe.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
-	import { OrbitControls } from '@threlte/extras';
+	import { OrbitControls, useTexture } from '@threlte/extras';
 
-	const globeRadius = 2;
+	// A larger radius allows for a more detailed normal map, which is important for the visual quality of the globe
+	const globeRadius = 128;
 
 	let autoRotate = $state(true);
+
+	const normalMap = useTexture('/normal-map.png');
 </script>
 
 <T.PerspectiveCamera
 	makeDefault
-	position={[7, 0, 0]}
+	position={[globeRadius * 3.5, 0, 0]}
 	// No matter where the camera moves, always keep the mesh centered
 	oncreate={(ref) => ref.lookAt(0, 0, 0)}
 >
@@ -18,16 +21,30 @@
 		autoRotateSpeed={1}
 		enableDamping
 		enablePan={false}
-		maxDistance={10}
+		maxDistance={globeRadius * 5}
 		minDistance={globeRadius + 0.1}
 		onstart={() => (autoRotate = false)}
 	/>
 </T.PerspectiveCamera>
 
-<T.DirectionalLight position={[10, 20, -25]} intensity={25} castShadow />
-<T.AmbientLight color="#d6efff" />
+<T.DirectionalLight
+	position={[50, 10, 50]}
+	intensity={3}
+	oncreate={(ref) => {
+		// Point the light exactly at the center of the globe
+		ref.lookAt(0, 0, 0);
+	}}
+/><T.AmbientLight intensity={0.2} color="#b9d5ff" />
 
-<T.Mesh>
-	<T.SphereGeometry args={[globeRadius, 32, 32]} />
-	<T.MeshStandardMaterial color="grey" />
-</T.Mesh>
+{#await normalMap then value}
+	<T.Mesh>
+		<T.SphereGeometry args={[globeRadius, 300, 300]} />
+		<T.MeshStandardMaterial
+			color="grey"
+			normalMap={value}
+			normalScale={[1, 1]}
+			roughness={0.5}
+			metalness={0.1}
+		/>
+	</T.Mesh>
+{/await}
