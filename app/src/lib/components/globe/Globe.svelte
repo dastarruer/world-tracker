d4e0cb990547a16292aab47946528489d343adb1
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-03-26 19:09:25 -0400

    Add loading state to globe

--- app/src/lib/components/globe/Globe.svelte
+++ app/src/lib/components/globe/Globe.svelte
@@ -1,13 +1,39 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
 	import { OrbitControls, useTexture } from '@threlte/extras';
+	import { Texture } from 'three';
 
 	// A larger radius allows for a more detailed normal map, which is important for the visual quality of the globe
 	const globeRadius = 12.8;
 
 	let autoRotate = $state(true);
 
-	const normalMap = useTexture('/normal-map.png');
+	// Before the map loads, use a low number of segments to make the wireframe have less lines
+	let numSegments = $state(20);
+
+	let normalMap = $state<Texture | null>(null);
+	let isLoaded = $state(false);
+
+	useTexture('/normal-map.png').then((map) => {
+		isLoaded = true;
+		normalMap = map;
+
+		setTimeout(() => {
+			numSegments = 128;
+		}, 100);
+	});
+
+	let materialProps = $derived({
+		color: '#050505',
+		// Use wireframe as a loading state
+		wireframe: !isLoaded,
+		transparent: !isLoaded,
+		opacity: isLoaded ? 1 : 0.3, // Fades the heavy wireframe so it looks better
+		normalMap,
+		normalScale: [2, 2] as [number, number],
+		roughness: isLoaded ? 0.8 : 1,
+		metalness: isLoaded ? 0.2 : 0
+	});
 </script>
 
 <T.PerspectiveCamera
@@ -40,15 +66,7 @@
 	<T.AmbientLight color="#b9d5ff" intensity={2} />
 </T.PerspectiveCamera>
 
-{#await normalMap then normalMap}
-	<T.Mesh>
-		<T.SphereGeometry args={[globeRadius, 450, 450]} />
-		<T.MeshStandardMaterial
-			color="#050505"
-			roughness={0.8}
-			metalness={0.2}
-			{normalMap}
-			normalScale={[2, 2]}
-		/>
-	</T.Mesh>
-{/await}
+<T.Mesh>
+	<T.SphereGeometry args={[globeRadius, numSegments, numSegments]} />
+	<T.MeshStandardMaterial {...materialProps} />
+</T.Mesh>
