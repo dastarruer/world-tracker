8e314194469bab5d12c725dd7eb49bafb24c91c8
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-03-28 20:25:02 -0400

    Add eslint config

---
+++ app/eslint.config.js
@@ -0,0 +1,25 @@
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+
+export default [
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	{
+		languageOptions: { globals: { ...globals.browser, ...globals.node } }
+	},
+
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parserOptions: { parser: ts.parser, extraFileExtensions: ['.svelte'] }
+		}
+	},
+	{ ignores: ['build/', '.svelte-kit/', 'dist/'] },
+	prettier,
+	svelte.configs.prettier,
+	svelte.configs.recommended
+];
